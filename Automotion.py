28d09ba980d225135f9d0db13545068fa8bf31d6
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-08-09 15:48:07 +0100

    Add files via upload

---
+++ Automotion.py
@@ -0,0 +1,32 @@
+print("Welcome to my hobby I like to do alot!")
+question1=input("Do you like personality quizzes? Y-N:")
+y=("y")
+n=("n")
+print(question1)
+
+a=("a")
+b=("10 points to you!")
+c=("")
+if (question1==y):
+    print("great! first of all you should know that these quizzes should be taken by the heart and not the head!")
+    print("And second of all the aim of these quizzes is to choose the one that you would prefer as in...")
+    print("would you rather:")
+    print(" a)Save a drowning dog.")
+    print(" b)Get 5 million dollars/pounds.")
+    print(" c)be able to crontol Fire.")
+    answer=input("choose a,b,c (type a,b or c):")
+    print(answer)
+    if (answer==a):
+        print("50 points to you!")
+    elif (answer==b):
+        print("10 points to you!")
+    else:
+        print("30 points to you!") 
+    print("My favourite quizzes are:")
+    print("1.Which dragon are you? (you can find it on youtube)")  
+    print("2.Which vampire are you? (also on youtube)")
+    print("3.Which is your soul element? (also on youtube)")
+    print("I hope you try one of them! It would make my day")
+    print("Hope you liked it Miss.Chanukya Buri. If you said yes, re-run the code and try no!")         
+else:
+    print("ok then :( ! I am not sad! Who said I am sad!!?? Have a good Day-Night-Afternoon-Evening or whatever the time is! If you said no, re-run the code and try yes!")    
